60f779ebe89bcc0c22bcf41abcb364ece872c1e1
antoniapiae <83601106+antoniapiae@users.noreply.github.com>
2021-12-01 21:31:12 +0100

    Added the search engine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import Dictionary from "./Dictionary";
+
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <div className="container">
+        <header className="App-header"></header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Antonia Englert</small>
+        </footer>
+      </div>
     </div>
   );
 }
