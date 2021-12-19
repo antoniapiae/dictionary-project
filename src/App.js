a3217785d2681994eb4c4fe8264037001951fa7d
antoniapiae <83601106+antoniapiae@users.noreply.github.com>
2021-12-19 22:41:30 +0100

    Final Adjustments

--- src/App.js
+++ src/App.js
@@ -7,13 +7,14 @@ export default function App() {
     <div className="App">
       <div className="container">
         <header className="App-header"></header>
-        <h1>Dictionary</h1>
+        <h1 className="header">Dictionary</h1>
         <main>
           <Dictionary defaultKeyword="meditation" />
         </main>
         <footer className="App-footer">
           <small>
             <a
+              className="source"
               href="https://github.com/antoniapiae/dictionary-project"
               target="_blank"
               rel="noreferrer"
